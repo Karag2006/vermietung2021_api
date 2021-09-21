a7ab86bce8e20e69676c5ab90e3c501d2364e267
Karag <karag2006@gmx.net>
2021-09-21 09:05:06 +0200

    UserController.php und User.php aktualisiert

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,12 +22,21 @@ class UserController extends Controller
     {
         // Validate the Input
         $this->validate($request, [
+            'name'      =>  'required|string|max:50',
+            'username'  =>  'required|string|unique:users|max:30',
+            'email'     =>  'required|string|email|max:191',
+            'password'  =>  'required|string|confirmed|min:6|max:191'
         ]);
 
-        $user = User::create($request->all());
+        $user = User::create([
+            'name' => $request['name'],
+            'username' => $request['username'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
 
         // for the Response limit the elements of the newly created User
-        // to those that are also transfered in the Ressource List.
+        // to those that are also transfered in the Ressource Index.
         $user = $user->only([
             'id',
             'name',
@@ -55,7 +64,18 @@ class UserController extends Controller
     {
         // Validate Input
         $this->validate($request, [
+            'name'      =>  'required|string|max:191',
+            // username is unique in the database.
+            // if the username is unchanged this would cause a database error
+            // so we have to exclude the username field of the current user from the unique validation
+            'username'  =>  'required|string|max:191|unique:users,username,' . $user->id,
+            'email'     =>  'required|string|email|max:191',
+            'password'  =>  'sometimes|string|confirmed|min:6|max:191'
         ]);
+
+        if (!empty($request->password)) {
+            $request->merge(['password' => Hash::make($request['password'])]);
+        }
 
         $user->update($request->all());
 
@@ -87,8 +107,18 @@ class UserController extends Controller
             'newPassword'   =>  'required|confirmed|min:6'
         ]);
 
+        // the current user is allowed to be here since he has a valid token
+        // the only way to know who the authenticated user actually is, is to look into the token
+        // and extract the custom claim "username" that was injected when the token was created.
+
         $token = JWTAuth::getToken();
         $username = JWTAuth::getPayload($token)->toArray()["username"];
+
+        // users can only change thier own password so we change the Password of the person named in the token.
+
+        // JWT Token would not be valid if someone messed with the contained data,
+        // so we can assume the person named in the token is in fact the token holder,
+        // to be sure, they need to confirm with their old valid password.
 
         $user = User::where('username', $username)->first();
         if (Hash::check($request['oldPassword'], $user->password)){
