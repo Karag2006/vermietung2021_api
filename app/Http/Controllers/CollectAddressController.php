977b4dcbfc09edc5f70308dcde26676168c64c3e
Karag <karag2006@gmx.net>
2021-10-05 18:41:40 +0200

    feature Init

---
+++ app/Http/Controllers/CollectAddressController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CollectAddress;
+use Illuminate\Http\Request;
+
+class CollectAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CollectAddress  $collectAddress
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CollectAddress $collectAddress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CollectAddress  $collectAddress
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CollectAddress $collectAddress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CollectAddress  $collectAddress
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CollectAddress $collectAddress)
+    {
+        //
+    }
+}
