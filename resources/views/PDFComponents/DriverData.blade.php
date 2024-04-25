14332eb39cef314a48148410ca0b94659b037f3f
Karag <karag2006@gmx.net>
2024-04-25 11:31:37 +0200

    Update: Rules for displaying "Driver Name" on the Document PDF -> Only one name will be displayed.

--- resources/views/PDFComponents/DriverData.blade.php
+++ resources/views/PDFComponents/DriverData.blade.php
@@ -1,7 +1,12 @@
 @if ($document->current_state == "contract")
     <tr>
         <td class="txt-bold">Fahrer:</td>
-        <td class="td-info">{{ $document->driver_name1 ?? $document->customer_name1 }} {{$document->driver_name2 ?? $document->customer_name2 }}</td>
+        @if(!$document->driver_name1)
+            <td class="td-info">{{ $document->customer_name2 ?? $document->customer_name1 }}</td>
+        @else
+            <td class="td-info">{{ $document->driver_name2 ?? $document->driver_name1 }}</td>
+        @endif
+
         <td class="txt-bold pl-4">FS Klasse & Nr.</td>
         <td class="td-info">{{ $document->driver_driving_license_class ?? $document->customer_driving_license_class }} &nbsp;	&nbsp;	&nbsp;	&nbsp;	| &nbsp;	&nbsp; {{ $document->driver_driving_license_no ?? $document->customer_driving_license_no }}</td>
     </tr>
